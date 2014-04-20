af364e5e99e01454984c72f560ca25dc61ae44b5
Ilya S <public.ilya.s@gmail.com>
2014-04-20 21:22:25 +0700

    add the very first test case

---
+++ spec/spec/Head/HeadSpec.php
@@ -0,0 +1,16 @@
+<?php namespace spec\Head;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class HeadSpec extends ObjectBehavior {
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Head\Head');
+    }
+
+
+
+}
+
---
+++ src/Head/Head.php
@@ -0,0 +1,8 @@
+<?php namespace Head;
+
+class Head {
+
+
+
+}
+
