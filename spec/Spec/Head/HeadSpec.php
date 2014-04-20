9ed5125352fdf6133455be61ce5e81aa6c34cf24
Ilya S <public.ilya.s@gmail.com>
2014-04-20 23:37:02 +0700

    write tests before the actual production code

--- spec/Spec/Head/HeadSpec.php
+++ spec/Spec/Head/HeadSpec.php
@@ -10,7 +10,23 @@ class HeadSpec extends ObjectBehavior {
         $this->shouldHaveType('Head\Head');
     }
 
+    function it_registers_an_event_handler()
+    {
+        $this->listen('foo', function(array $context)
+        {
 
+        });
+    }
+
+    function it_fires_an_event()
+    {
+        $this->listen('foo', function(array $context)
+        {
+            throw new $context[1];
+        });
+
+        $this->shouldThrow($e = 'LogicException')->duringFire('foo', [null, $e]);
+    }
 
 }
 
